64be82fca82ccf4369ecca29045e67dc926d121e
Steven Halase <steven.halase@gmail.com>
2016-07-13 19:35:54 -0600

    Refactor

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  gClientID : '599988447746-uektkf1gld8cn32f9udqbh404guo6qp0.apps.googleusercontent.com',
-  gSecret : 'Tahl05klG9Vx0ImLOXZDUl5P',
-  sKey  : '5E9C274F1883D1017D2D677F0DD21F3C'
-}
