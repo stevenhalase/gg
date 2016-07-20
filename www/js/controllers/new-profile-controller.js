3f7d8e1f7b6ddaeffdbed58e2da9a47b1990ca37
Steven Halase <steven.halase@gmail.com>
2016-07-20 09:17:47 -0600

    Adjusted newProfile

--- www/js/controllers/new-profile-controller.js
+++ www/js/controllers/new-profile-controller.js
@@ -11,18 +11,22 @@ function newProfileController($state, $http, userFactory) {
   console.log(npCtrl.currentUser);
 
   npCtrl.searchFavoriteGame = function() {
-    console.log(npCtrl.favoriteGameSearchField);
-    $http.get('https://api.twitch.tv/kraken/search/games?q=' + npCtrl.favoriteGameSearchField + '&type=suggest')
+    console.log(dCtrl.favoriteGameSearchField);
+    $http.get('https://api.twitch.tv/kraken/search/games?q=' + dCtrl.favoriteGameSearchField + '&type=suggest')
       .then(function(response) {
         // console.log(response)
         if(response.data.games.length > 0) {
+          dCtrl.favoriteGameSearchField = '';
+          Materialize.toast('Game added!', 3000)
           // console.log('game result: ', response.data.games[0])
-          npCtrl.currentUser.favoriteGames.push(response.data.games[0]);
-          console.log(npCtrl.currentUser.favoriteGames[0]);
-          $http.post('/api/me', npCtrl.currentUser)
+          dCtrl.currentUser.favoriteGames.push(response.data.games[0]);
+          console.log(dCtrl.currentUser.favoriteGames[0]);
+          $http.post('/api/me', dCtrl.currentUser)
             .then(function(response) {
               console.log('SAVED DUDE: ', response);
             });
+        } else {
+          Materialize.toast('Game not found!', 3000)
         }
       });
   };
