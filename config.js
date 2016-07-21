aa151624a56f70e658d8cf44697d5e927ca1decb
Steven Halase <steven.halase@gmail.com>
2016-07-21 10:42:19 -0600

    Fixed Steam login

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   gClientID : '599988447746-uektkf1gld8cn32f9udqbh404guo6qp0.apps.googleusercontent.com',
   gSecret : 'Tahl05klG9Vx0ImLOXZDUl5P',
-  sKey  : '5E9C274F1883D1017D2D677F0DD21F3C'
+  sKey  : '62C592C47F3B665D212D83B114C08414'
 }
